08b6418ddda76198b0880ddb6b67c3df2693012b
elazsyahranie <elazaribrahims@gmail.com>
2026-02-12 16:52:28 +0700

    initial design

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,25 @@ function App() {
   return (
     <>
       <div className="grid h-screen">
-        {/* <div className="flex flex-col"> */}
-        {/* <div className="my-auto"> */}
         <div className="m-auto">
-          <h1 className="text-5xl text-center font-bold text-teal-700">
+          <h1 className="text-8xl text-center font-bold text-teal-700 text-shadow-lg/30 mb-2">
             Kiosk.
           </h1>
-          <h1 className="text-center font-medium">Manage your store at ease</h1>
+          <h1 className="text-center text-xl font-medium mb-2">
+            Manage your store at ease
+          </h1>
+          <div className="flex justify-center mb-2">
+            <div className="rounded-sm bg-sky-700 hover:bg-sky-800 min-w-32 grid justify-center items-center text-sky-50 h-8 me-3">
+              <a href="#">Register</a>
+            </div>
+            <div className="rounded-sm bg-sky-700 hover:bg-sky-800 min-w-32 grid justify-center items-center text-sky-50 h-8 ms-3">
+              <a href="#">Log In</a>
+            </div>
+          </div>
+          <div className="rounded-sm shadow-lg shadow-gray-400 hover:bg-gray-200 min-w-32 grid justify-center items-center h-8">
+            <span className="text-gray-900 font-medium">Google</span>
+          </div>
         </div>
-        {/* </div> */}
-        {/* </div> */}
       </div>
     </>
   );
