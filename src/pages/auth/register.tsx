7bdadc9769a5c94a35899f23f051617607caaaac
elazsyahranie <elazaribrahims@gmail.com>
2026-02-16 20:47:55 +0700

    button design improved

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -88,7 +88,7 @@ function Register() {
               <input
                 type="submit"
                 value="Submit"
-                className="rounded-sm bg-sky-700 hover:bg-sky-800 w-full grid justify-center items-center text-sky-50 h-10 me-3"
+                className="rounded-sm bg-sky-700 hover:bg-sky-800 w-full grid justify-center items-center text-sky-50 h-12 me-3 font-medium"
               ></input>
             </form>
           </div>
