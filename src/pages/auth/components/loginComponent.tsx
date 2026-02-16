ef699c12887601db567bb7d548b94ab4092a0cfa
elazsyahranie <elazaribrahims@gmail.com>
2026-02-16 22:32:33 +0700

    login

---
+++ src/pages/auth/components/loginComponent.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import type { ChangeEvent } from 'react';
+
+function LoginComponent() {
+  type FormData = {
+    phoneEmailUsername: string;
+    password: string;
+  };
+
+  const [formData, setFormData] = useState<FormData>({
+    phoneEmailUsername: '',
+    password: '',
+  });
+
+  const formLabels = [
+    { label: 'Phone/Email/Username', id: 'phoneEmailUsername', type: 'text' },
+    { label: 'Password', id: 'password', type: 'password' },
+  ];
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
+    // Prevent the default page reload
+    event.preventDefault();
+    // You can add validation, API calls, etc. here
+    console.dir(formData, { depth: null });
+    // Optional: Reset the form field
+    setFormData(() => ({
+      phoneEmailUsername: '',
+      password: '',
+    }));
+  };
+
+  return (
+    <>
+      <div className="w-[80%] pt-20">
+        <h1 className="text-4xl font-medium text-center mb-4">Login</h1>
+        <form onSubmit={handleSubmit} className="mb-4">
+          <div className="mb-4">
+            {formLabels.map((item, index) => {
+              return (
+                <div key={index}>
+                  <label htmlFor={item.id} className="text-sm">
+                    {item.label}
+                  </label>
+                  <br></br>
+                  <input
+                    type={item.type}
+                    id={item.id}
+                    name={item.id}
+                    // defaultValue={item.label}
+                    onChange={(e) => handleChange(e)}
+                    className="border border-gray-300 rounded-sm w-full h-9 mb-2"
+                  ></input>
+                  <br></br>
+                </div>
+              );
+            })}
+          </div>
+          <input
+            type="submit"
+            value="Submit"
+            className="rounded-sm bg-sky-700 hover:bg-sky-800 w-full grid justify-center items-center text-sky-50 h-12 me-3 font-medium"
+          ></input>
+        </form>
+
+        <div className="flex items-center mb-4">
+          <div className="flex-1 border-t border-gray-300"></div>
+          <span className="px-4 text-gray-500 text-sm">OR</span>
+          <div className="flex-1 border-t border-gray-300"></div>
+        </div>
+
+        <div className="rounded-sm border border-gray-400 bg-gray-100 hover:bg-gray-300 w-full grid justify-center items-center h-12 me-3 font-medium text-gray-700 mb-4">
+          Continue with Google
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LoginComponent;
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,7 +1,16 @@
+import LoginComponent from './components/loginComponent';
+// import RegisterComponent from './components/registerComponent';
+// h200
+
 function Login() {
   return (
     <>
-      <h1>Login</h1>
+      <div className="flex h-screen">
+        <div className="w-[50vw] bg-teal-700"></div>
+        <div className="w-[50vw] flex justify-center overflow-visible">
+          <LoginComponent />
+        </div>
+      </div>
     </>
   );
 }
