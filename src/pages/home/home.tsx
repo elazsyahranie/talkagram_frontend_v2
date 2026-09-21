13421efb717920e7663a1461dd118cf98f597ee2
elazsyahranie <elazaribrahims@gmail.com>
2026-09-21 21:05:16 +0700

    chat name displayed

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,24 +1,33 @@
 import { InlineIcon } from '@iconify/react';
+import { useState } from 'react';
+// import type { ChangeEvent } from 'react';
 import './home.css';
 
 function Home() {
+  const [chat, setChat] = useState('');
+  console.log(chat);
+
+  // const handleChooseChat = (event: ChangeEvent<HTMLFormElement>) => {
+
+  // };
+
   const chatsList = [
-    { name: 'Adrian', city: 'Jakarta' },
-    { name: 'Maya', city: 'Bandung' },
-    { name: 'Ethan', city: 'Surabaya' },
-    { name: 'Nadia', city: 'Yogyakarta' },
-    { name: 'Julian', city: 'Medan' },
-    { name: 'Clara', city: 'Semarang' },
-    { name: 'Rafael', city: 'Makassar' },
-    { name: 'Sophie', city: 'Denpasar' },
-    { name: 'Dylan', city: 'Malang' },
-    { name: 'Amara', city: 'Palembang' },
+    { id: 1, name: 'Adrian', city: 'Jakarta' },
+    { id: 2, name: 'Maya', city: 'Bandung' },
+    { id: 3, name: 'Ethan', city: 'Surabaya' },
+    { id: 4, name: 'Nadia', city: 'Yogyakarta' },
+    { id: 5, name: 'Julian', city: 'Medan' },
+    { id: 6, name: 'Clara', city: 'Semarang' },
+    { id: 7, name: 'Rafael', city: 'Makassar' },
+    { id: 8, name: 'Sophie', city: 'Denpasar' },
+    { id: 9, name: 'Dylan', city: 'Malang' },
+    { id: 10, name: 'Amara', city: 'Palembang' },
   ];
 
   return (
     <>
-      <div className="flex h-screen">
-        <div className="w-[30%] h-screen text-white bg-gray-800 border-r border-gray-500 overflow-hidden">
+      <div className="flex text-white h-screen">
+        <div className="w-[30%] h-screen  bg-gray-800 border-r border-gray-500 overflow-hidden">
           <h1 className="text-4xl ms-2 my-3 font-semibold">Talkagram</h1>
           <div className="flex ms-2 me-4">
             <InlineIcon
@@ -43,7 +52,8 @@ function Home() {
                 return (
                   <div
                     key={index}
-                    className="h-18 rounded-lg flex items-center hover:bg-gray-700 mb-2 max-w-88"
+                    className="h-18 rounded-lg flex items-center hover:bg-gray-700 hover:cursor-pointer mb-2 max-w-88"
+                    onClick={() => setChat(item.name)}
                   >
                     {/* <h2 className="text-center my-auto min-w-12">Prof</h2> */}
                     <InlineIcon
@@ -63,7 +73,21 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className="w-[80%] bg-gray-700">{/* <h1>Right</h1> */}</div>
+        <div className="w-[80%] bg-gray-700">
+          {/* <h1>Right</h1> */}
+          {chat && (
+            <div className="bg-gray-800 h-21">
+              <div className="mx-8 h-full flex items-center">
+                {/* <h1>Name here</h1> */}
+                <InlineIcon
+                  icon="iconamoon:profile-circle-fill"
+                  className="text-6xl my-auto mr-2 text-gray-400 min-w-12"
+                />
+                <h2 className="text-lg font-medium">{chat}</h2>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
